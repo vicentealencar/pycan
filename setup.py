a19b614c0c48504e3cbe83f6b072844fe685f19e
Vicente Alencar <vicente@jusbrasil.com.br>
2014-02-01 19:27:37 -0300

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__version__ = '0.1.0'
+
+setup(
+    name='pyfunct',
+    version=__version__,
+    url='https://github.com/jusbrasil/pycan',
+    author=u'Vicente Carlos de Alencar Jr',
+    packages=find_packages(),
+    include_package_data=True,
+    tests_require=['mock'],
+)
