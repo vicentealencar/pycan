a3b7558bf327c714c5612fc94388dc2fa07c47b7
matchs <contato@mateuschagas.com.br>
2014-02-05 16:25:48 -0300

    Feature: Adding support to array of actions. Closes #1   - Allowing to add a list of actions to a list of targets   - Tests added
    Enhancement: Check for '*' permitted actions. Closes #2
      - Checking if it's not permitted to add a list of actions with an '*' in it
      - Checking if it's not permitted to add an action to a target that already has '*'
      - Checking if it's not permitted to add an '*' to a target that already has actions
      - Test added
    
    Enhancement: Adding custom Exceptions
      - Unauthorized resource
      - Action not found
      - Target not found
      - Action already exists
      - Target already has '*'
      - Target already has actions
      - Action list with '*' in it

--- pycan/__init__.py
+++ pycan/__init__.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 import inspect
+import exceptions
 
 _permissions = {}
 
-def _default_exception():
-  return Exception("Unauthorized access")
-
-def can(action, target, authorization, get_authorization_resource=lambda _,__: None,
+def can(actions, targets, authorization, get_authorization_resource=lambda _,__: None,
   get_resource=lambda _,__: None, exception=None):
-  assert action, "An action must be specified"
-  assert target, "A target must be specified"
+  assert actions, "An action must be specified"
+  assert targets, "A target must be specified"
   assert authorization, "An authorization procedure must be specified"
   assert getattr(get_authorization_resource, '__call__'), "get_authorization_resource must be callable"
   assert getattr(get_resource, '__call__'), "get_resource must be callable"
@@ -19,18 +17,35 @@ def can(action, target, authorization, get_authorization_resource=lambda _,__: N
   assert len(inspect.getargspec(get_resource).args) == 2, "get_resource must accept 2 parameters"
   assert len(inspect.getargspec(authorization).args) == 3, "authorization must accept 3 parameters"
 
-  if target not in _permissions:
-    _permissions[target] = {}
+  if not _is_sequence(targets):
+    targets = [targets]
 
-  if action in _permissions[target] or "*" in _permissions[target]:
-    raise Exception("A permission for this target resource has already been specified")
+  if not _is_sequence(actions):
+    actions = [actions]
+  elif "*" in actions and len(actions) > 1:
+    raise exceptions.ActionListWithAsteriskError("* must be a solo action")
 
-  _permissions[target][action] = {
-      'exception': exception or _default_exception(),
-      'authorization': authorization,
-      'get_authorization_resource': get_authorization_resource,
-      'get_resource': get_resource,
-    }
+  for target in targets:
+    if target not in _permissions:
+      _permissions[target] = {}
+
+    if "*" in _permissions[target]:
+      raise exceptions.TargetAlreadyHasAsteriskError("The target \"%s\" already has an \"*\"" % target)
+
+    for action in actions:
+      if action == "*" and len(_permissions[target]) > 0:
+        raise exceptions.TargetAlreadyHasActionsError("Can't register \"*\" cause the target \"%s\" already has actions" % target)
+
+      if action in _permissions[target]:
+        raise exceptions.ActionAlreadyExistsError("A permission for this target resource has already been specified")
+
+      _permissions[target][action] = {
+          'exception': exception,
+          'authorization': authorization,
+          'get_authorization_resource': get_authorization_resource,
+          'get_resource': get_resource,
+        }
+    
 
 def can_i(action, target, user, context=None):
   assert action, "An action must be specified"
@@ -63,4 +78,9 @@ def authorize(action, target, user, context=None):
   if go_ahead:
     return auth_resource, resource
   else:
-    raise ((_permissions.get(target) or {}).get(action) or {}).get("exception") or _default_exception()
+    raise ((_permissions.get(target) or {}).get(action) or {}).get("exception") or UnauthorizedResourceError(action, target, user, context, resource, _permissions[target][action])
+
+def _is_sequence(arg):
+    return (not hasattr(arg, "strip") and
+            hasattr(arg, "__getitem__") or
+            hasattr(arg, "__iter__"))
