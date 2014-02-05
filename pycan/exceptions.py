e0c97689b9b0f426579443208d85487b5e985f8d
matchs <contato@mateuschagas.com.br>
2014-02-05 16:51:02 -0300

    Minot style fixing and identation

--- pycan/exceptions.py
+++ pycan/exceptions.py
@@ -1,38 +1,38 @@
 class UnauthorizedResourceError(Exception):
-	def __init__(self, action, target, user, context, resource, permission):
-		self.action = action
-		self.target = target
-		self.user = user
-		self.context = context
-		self.resource = resource
-		self.permission = permission
+  def __init__(self, action, target, user, context, resource, permission):
+    self.action = action
+    self.target = target
+    self.user = user
+    self.context = context
+    self.resource = resource
+    self.permission = permission
 
 
 class ActionNotFoundError(Exception):
-	def __init__(self, value):
-		self.value = value
+  def __init__(self, value):
+    self.value = value
 
 
 class TargetNotFoundError(Exception):
-	def __init__(self, value):
-		self.value = value
+  def __init__(self, value):
+    self.value = value
 
 
 class ActionAlreadyExistsError(Exception):
-	def __init__(self, value):
-		self.value = value
+  def __init__(self, value):
+    self.value = value
 
-		
+    
 class TargetAlreadyHasAsteriskError(Exception):
-	def __init__(self, value):
-		self.value = value
+  def __init__(self, value):
+    self.value = value
 
 
 class TargetAlreadyHasActionsError(Exception):
-	def __init__(self, value):
-		self.value = value
-		
+  def __init__(self, value):
+    self.value = value
+
 
 class ActionListWithAsteriskError(Exception):
-	def __init__(self, value):
-		self.value = value
+  def __init__(self, value):
+    self.value = value
--- tests/unit.py
+++ tests/unit.py
@@ -3,99 +3,76 @@ import pycan
 
 class CanTest(unittest.TestCase):
   def setUp(self):
-  	pycan._permissions = {}
+    pycan._permissions = {}
 
-#	def tearDown(self):
+  def can_no_target_test(self):
+    pass
 
+  def can_single_target_test(self):
+    pass
 
-#	def can_no_target_test(self):
+  def can_array_targets_test(self):
+    pass
 
-
-#	def can_single_target_test(self):
-
-
-#	def can_array_targets_test(self):
-
-
-#	def can_no_action_test(self):
+  def can_no_action_test(self):
+    pass
 
   def assert_action_was_stored(self, action, target, auth):
-	self.assertIn(target, pycan._permissions)
-	self.assertIn(action, pycan._permissions[target])
-	self.assertEqual(pycan._permissions[target][action]['authorization'], auth)
+    self.assertIn(target, pycan._permissions)
+    self.assertIn(action, pycan._permissions[target])
+    self.assertEqual(pycan._permissions[target][action]['authorization'], auth)
 
   def can_single_action_test(self):
-	target = 'target'
-	action = 'action'
-	auth = lambda u, r, c: True
-	pycan.can(action, target, auth)
-	self.assert_action_was_stored(action, target, auth)
+    target = 'target'
+    action = 'action'
+    auth = lambda u, r, c: True
+    pycan.can(action, target, auth)
+    self.assert_action_was_stored(action, target, auth)
 
-	targets = ['targetOne', 'targetTwo', 'targetThree']
-	pycan.can(action, targets, auth)
-	for target in targets:
-	  self.assert_action_was_stored(action, target, auth)
+    targets = ['targetOne', 'targetTwo', 'targetThree']
+    pycan.can(action, targets, auth)
+    for target in targets:
+      self.assert_action_was_stored(action, target, auth)
 
   def can_array_actions_test(self):
-	target = 'target'
-	actions = ['actionOne', 'actionTwo', 'actionThree']
-	auth = lambda u, r, c: True
-	pycan.can(actions, target, auth)
-	for action in actions:
-	  self.assert_action_was_stored(action, target, auth)
+    target = 'target'
+    actions = ['actionOne', 'actionTwo', 'actionThree']
+    auth = lambda u, r, c: True
+    pycan.can(actions, target, auth)
+    for action in actions:
+      self.assert_action_was_stored(action, target, auth)
 
-  	targets = ['targetOne', 'targetTwo', 'targetThree']
-  	pycan.can(actions, targets, auth)
-  	for target in targets:
-  	  for action in actions:
-	  	self.assert_action_was_stored(action, target, auth)
+    targets = ['targetOne', 'targetTwo', 'targetThree']
+    pycan.can(actions, targets, auth)
+    for target in targets:
+      for action in actions:
+        self.assert_action_was_stored(action, target, auth)
 
   def can_asterisk_test(self):
-  	target = 'target'
-  	actions = ['actionA','*']
-  	auth = lambda u, r, c: True
-  	self.assertRaises(pycan.exceptions.ActionListWithAsteriskError, pycan.can, actions, target, auth)
+    target = 'target'
+    actions = ['actionA','*']
+    auth = lambda u, r, c: True
+    self.assertRaises(pycan.exceptions.ActionListWithAsteriskError, pycan.can, actions, target, auth)
 
-  	pycan.can('action', target, auth)
-  	self.assertRaises(pycan.exceptions.TargetAlreadyHasActionsError, pycan.can, '*', target, auth)
+    pycan.can('action', target, auth)
+    self.assertRaises(pycan.exceptions.TargetAlreadyHasActionsError, pycan.can, '*', target, auth)
 
-  	target = 'targetTwo'
-  	pycan.can('*', target, auth)
-  	self.assertRaises(pycan.exceptions.TargetAlreadyHasAsteriskError, pycan.can, 'action', target, auth)
-
-
-
-#	def can_authorization_test(self):
+    target = 'targetTwo'
+    pycan.can('*', target, auth)
+    self.assertRaises(pycan.exceptions.TargetAlreadyHasAsteriskError, pycan.can, 'action', target, auth)
 
 
-#	def can_get_authorization_resource_test(self):
+  def can_authorization_test(self):
+    pass
 
+  def can_get_authorization_resource_test(self):
+    pass
 
-#	def can_get_resource_test(self):
+  def can_get_resource_test(self):
+    pass
 
+  def can_exception_test(self):
+    pass
 
-#	def can_exception_test(self):
-		
-
-#	def can_full_test(self):
-
-
-
-
-#class CanITest(unittest.TestCase):
-
-
-#class AuthorizeTest(unittest.TestCase)
-
-
-#class ExceptionTest(unittest.TestCase):
-#	def setUp(self):
-
-
-#	def tearDown(self):
-
-
-#	def default_exception_test(self):
-
-
-#	def default_exception_customisation_test(self):
+  def can_full_test(self):
+    pass
